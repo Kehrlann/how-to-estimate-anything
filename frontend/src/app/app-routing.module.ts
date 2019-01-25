191553dfc8631aa2127488df2bcd073d847a281d
Daniel Garnier-Moiroux <git@garnier.wf>
2019-01-25 17:42:02 +0100

    Add route to display questions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'question/:id',
+    component: QuestionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
