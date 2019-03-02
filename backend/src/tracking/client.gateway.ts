e15d537690fc0a026efda6dae9b1318a16364f21
Daniel Garnier-Moiroux <git@garnier.wf>
2019-03-02 19:38:47 +0100

    Remove unused websocket client code, namespace the gateway

--- backend/src/tracking/client.gateway.ts
+++ backend/src/tracking/client.gateway.ts
@@ -1,19 +1,13 @@
 import {
-  SubscribeMessage,
-  WebSocketGateway,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  WebSocketGateway,
 } from '@nestjs/websockets';
 import { TrackingService } from './tracking.service';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: 'client' })
 export class ClientGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private trackingService: TrackingService) {}
-
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
 
   handleConnection(client: any, ...args: any[]) {
     this.trackingService.addClient();
