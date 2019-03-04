bb9b10e54a685d501a25b5cfe2600dbc47846b58
Daniel Garnier-Moiroux <git@garnier.wf>
2019-03-04 23:03:47 +0100

    Add the Websocket gateway for admin users

---
+++ backend/src/tracking/admin.gateway.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AdminGateway } from './admin.gateway';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { TrackingService } from './tracking.service';
+
+describe('AdminGateway', () => {
+  let gateway: AdminGateway;
+  let clientCountSubject: BehaviorSubject<number>;
+
+  beforeEach(async () => {
+    clientCountSubject = new BehaviorSubject<number>(0);
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AdminGateway, TrackingService],
+    })
+      .overrideProvider(TrackingService)
+      .useValue({ clientCount$: clientCountSubject })
+      .compile();
+
+    gateway = module.get<AdminGateway>(AdminGateway);
+  });
+
+  it('should be defined', () => {
+    expect(gateway).toBeDefined();
+  });
+
+  describe('sending client count', () => {
+    it('sends clientCount to the admin client', () => {
+      const server = { emit: jest.fn() };
+      gateway.afterInit(server);
+      clientCountSubject.next(42);
+
+      expect(server.emit).toHaveBeenCalledWith('client_count', 42);
+    });
+
+    it('sends clientCount when the admin connects, but nothing after that', () => {
+      const client = { emit: jest.fn() };
+
+      clientCountSubject.next(24);
+      gateway.handleConnection(client);
+      clientCountSubject.next(42);
+
+      expect(client.emit).toHaveBeenCalledWith('client_count', 24);
+      expect(client.emit).toHaveBeenCalledTimes(1);
+    });
+  });
+});
---
+++ backend/src/tracking/admin.gateway.ts
@@ -0,0 +1,25 @@
+import {
+  OnGatewayInit,
+  WebSocketGateway,
+  OnGatewayConnection,
+} from '@nestjs/websockets';
+import { TrackingService } from './tracking.service';
+import { take } from 'rxjs/operators';
+
+@WebSocketGateway({ namespace: 'admin' })
+export class AdminGateway implements OnGatewayInit, OnGatewayConnection {
+  constructor(private trackingService: TrackingService) {}
+
+  afterInit(server: any) {
+    this.trackingService.clientCount$.subscribe(count =>
+      server.emit('client_count', count),
+    );
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    // this works because clientCount$ is a BehaviorSubject and will give the last known value
+    this.trackingService.clientCount$
+      .pipe(take(1))
+      .subscribe(count => client.emit('client_count', count));
+  }
+}
