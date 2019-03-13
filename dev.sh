5b629c88ecb72856d1613a248eb3fc5c09e03f51
Daniel Garnier-Moiroux <git@garnier.wf>
2019-03-13 08:06:38 +0100

    update dev.sh: attach tmux config in new window

--- dev.sh
+++ dev.sh
@@ -3,6 +3,9 @@
 SESSION="estimates"
 
 tmux new-session -d -s "$SESSION"
+
+# RUN
+tmux rename-window "run"
 tmux split-window -h
 tmux select-pane -t 0
 tmux send-keys "cd backend" C-m
@@ -11,3 +14,11 @@ tmux send-keys "yarn start:dev" C-m
 tmux select-pane -t 1
 tmux send-keys "cd frontend" C-m
 tmux send-keys "yarn start" C-m
+
+# EDIT
+tmux new-window -n "edit"
+tmux select-window -t "$SESSION:edit"
+
+
+# ATTACH
+tmux attach-session -t "$SESSION"
