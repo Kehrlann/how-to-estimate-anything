f87331788bf91ab2e13f783bc0b6eb4262f4d44b
Daniel Garnier-Moiroux <git@garnier.wf>
2019-04-03 15:56:47 +0200

    Some visual improvements to better work on mobile

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -15,16 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(
-      compiled
-        .querySelector('h1')
-        .textContent.toLowerCase()
-        .trim()
-    ).toContain('test de calibrage');
-  });
 });
